51b3e109335682103ff36599b13d1f09e75b165e
Max Hayashi Batista <rm563717@fiap.com.br>
2026-03-06 09:15:41 -0300

    feat: added API fetch to useEffect

--- App.js
+++ App.js
@@ -4,16 +4,24 @@ import { useState, useEffect } from "react";
 
 export default function App() {
   const [contador, setContador] = useState(0);
+  const [usuario, setUsuario] = useState([]); // irá armazenar o usuário vindo da API
 
   // Estruturação do useEffect()
   useEffect(() => {
-    console.log("Executando de acordo com alteração do estado...")
-  }, [contador])
+    async function buscarUsuarios() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1",
+      );
+      const data = await response.json();
+      setUsuario(data); // armazenando dados da api no estado
+    }
+
+    buscarUsuarios(); // chama função desenvolvida acima
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.fontG}>Valor: {contador}</Text>
-      <Button title="Aumentar" onPress={() => setContador(contador + 1)} />
+      <Text style={styles.fontG}>{usuario.company.name}</Text>
       <StatusBar style="auto" />
     </View>
   );
